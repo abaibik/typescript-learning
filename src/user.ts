0ac89bff79b879ee29e3ffe77db576b6a65546b7
Aleksandra Baibik <abaibik@gmail.com>
2022-01-19 22:11:46 +0100

    task1

--- src/user.ts
+++ src/user.ts
@@ -1,10 +1,43 @@
 import { renderBlock } from "./lib";
 
-export function renderUserBlock(
-  favoriteItemsAmount: number,
-  userName: string,
-  avatar: string
-) {
+function getData(key: string, defaultValue: unknown): string {
+  const value = localStorage.getItem(key);
+  if (value) return value;
+  switch (typeof defaultValue) {
+    case "number":
+      return `${defaultValue}`;
+    case "object":
+      return JSON.stringify(defaultValue);
+    default:
+      throw new Error("Unsupported type");
+  }
+}
+
+interface UserData {
+  username: string;
+  avatarUrl: string;
+}
+
+function getUserData(): UserData {
+  const user = JSON.parse(
+    getData("user", {
+      username: "Wade Warren",
+      avatarUrl: "/img/avatar.png",
+    })
+  );
+  return user;
+}
+
+function getFavoritesAmount(): number {
+  const favoritesAmount = parseInt(getData("favoritesAmount", 0));
+  return favoritesAmount;
+}
+
+export function renderUserBlock() {
+  const favoriteItemsAmount = getFavoritesAmount();
+
+  const { username, avatarUrl } = getUserData();
+
   const favoritesCaption = favoriteItemsAmount
     ? favoriteItemsAmount
     : "ничего нет";
@@ -14,9 +47,9 @@ export function renderUserBlock(
     "user-block",
     `
     <div class="header-container">
-      <img class="avatar" src="${avatar}" alt="${userName}" />
+      <img class="avatar" src="${avatarUrl}" alt="${username}" />
       <div class="info">
-          <p class="name">${userName}</p>
+          <p class="name">${username}</p>
           <p class="fav">
             <i class="heart-icon${
               hasFavoriteItems ? " active" : ""
